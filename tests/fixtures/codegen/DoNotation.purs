88d9368a6cc041b062baa0cfb952ea1504ae7e16
Rory Campbell <roryc89@googlemail.com>
2026-03-12 12:52:18 +0100

    test do notiation code gen

--- tests/fixtures/codegen/DoNotation.purs
+++ tests/fixtures/codegen/DoNotation.purs
@@ -1,11 +1,32 @@
 module DoNotation where
 
-class MyBind m where
-  myBind :: forall a b. m a -> (a -> m b) -> m b
+-- Bind class (needed for do notation desugaring)
+class Bind m where
+  bind :: forall a b. m a -> (a -> m b) -> m b
 
-class MyPure m where
-  myPure :: forall a. a -> m a
+-- Pure/Applicative for pure in do blocks
+class Pure m where
+  pure :: forall a. a -> m a
 
--- Simple do block simulation via bind chains
-bindChain :: forall m a. MyBind m => MyPure m => m a -> m a
-bindChain x = myBind x (\a -> myPure a)
+-- discard is needed for do-notation statements without <-
+discard :: forall f a. Bind f => f a -> (a -> f a) -> f a
+discard = bind
+
+-- Simple do block with bind
+doSimple :: forall m a. Bind m => m a -> (a -> m a) -> m a
+doSimple x f = do
+  a <- x
+  f a
+
+-- Do block with multiple binds
+doChain :: forall m. Bind m => Pure m => m Int -> m Int
+doChain x = do
+  a <- x
+  b <- pure a
+  pure b
+
+-- Do block with discard (statement without <-)
+doDiscard :: forall m. Bind m => m Int -> m Int -> m Int
+doDiscard x y = do
+  x
+  y
