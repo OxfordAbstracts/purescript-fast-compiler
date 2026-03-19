bc5cafd6b2961eee36bc52603aeb3ba5453ba2fd
Rory Campbell <roryc89@googlemail.com>
2026-03-19 12:17:08 +0100

    only show release mode for build_from_sources

--- tests/build.rs
+++ tests/build.rs
@@ -1229,8 +1229,7 @@ fn build_all_packages() {
     );
 }
 
-// run with: RUST_LOG=debug cargo test --test build build_from_sources -- --exact --ignored
-// for release (RECOMMENDED): cargo test --release --test build build_from_sources -- --exact --ignored --no-capture
+// run with: cargo test --release --test build build_from_sources -- --exact --ignored --no-capture
 #[test]
 #[ignore]
 #[timeout(600000)] // 10 min timeout
